477c8840832a74cbf3681934c22de6d48891f1dc
Daniel555AS <daniel57orlando@gmail.com>
2025-05-03 20:09:59 -0500

    Actualización general del sitio web para agregar los videojuegos y páginas referentes a cada proyecto

--- scripts/home.js
+++ scripts/home.js
@@ -7,7 +7,34 @@ document.addEventListener("DOMContentLoaded", function () {
                 secondSection.classList.add("show");
             }
         });
-    }, { threshold: 0.3 }); // Se activa cuando al menos el 30% de la sección sea visible
+    }, { threshold: 0.3 }); // Activated when at least 30% of the section is visible
 
     observer.observe(secondSection);
 });
+
+const thirdSection = document.querySelector(".third-section");
+
+const observerThirdSection = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            thirdSection.classList.add("show");
+        }
+    });
+}, { threshold: 0.3 }); // // Activated when at least 30% of the section is visible
+
+observerThirdSection.observe(thirdSection);
+
+// Select all project cards
+const projectCards = document.querySelectorAll('.project-card');
+
+// Add a click event to each card
+projectCards.forEach(card => {
+    card.addEventListener('click', function() {
+        // Gets the URL from the 'data-link' attribute
+        const link = card.getAttribute('data-link');
+        // Redirects to the URL
+        window.location.href = link;
+    });
+});
+
+
